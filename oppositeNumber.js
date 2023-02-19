d010e14f14287780b014b01ae780fae2ac845cdf
BK Pecho <bkpecho05@gmail.com>
2023-02-20 06:55:47 +0800

    oppositeNumbers

---
+++ oppositeNumber.js
@@ -0,0 +1,15 @@
+/* 
+
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+
+*/
+
+function opposite(number) {
+    return -number;
+  }
