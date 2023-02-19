d022e0d83bc7b1560639fbc4089172097d1e126b
BK Pecho <bkpecho05@gmail.com>
2023-02-20 06:30:28 +0800

    evenOrOdd && multiply

---
+++ evenOrOdd.js
@@ -0,0 +1,15 @@
+/* 
+
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+*/
+
+// Answer
+
+evenOrOdd = function (number) {
+  if (number % 2 === 0) {
+    return 'Even';
+  } else {
+    return 'Odd';
+  }
+};
---
+++ multiply.js
@@ -0,0 +1,9 @@
+/* 
+
+This code does not execute properly. Try to figure out why.
+
+*/
+
+function multiply(a, b) {
+  return a * b;
+}
