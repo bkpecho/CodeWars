797a3dd0bab6f677a5f2586fd66b2e1fc2654f62
woprandis09 <bkpecho05@gmail.com>
2023-02-24 22:40:57 +0800

    booleanValuesYesNo

---
+++ booleanValuesYesNo.js
@@ -0,0 +1,9 @@
+/* 
+
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+*/
+
+const boolToWord = function (bool) {
+  return bool === true ? 'Yes' : 'No';
+};
