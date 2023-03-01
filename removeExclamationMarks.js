105d6af4ac61885b3236443ec96aba3adb59b778
BK Pecho <bkpecho05@gmail.com>
2023-03-01 20:17:47 +0800

    removeExclamationMarks

---
+++ removeExclamationMarks.js
@@ -0,0 +1,9 @@
+/* 
+
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+*/
+
+function removeExclamationMarks(s) {
+  return s.replaceAll('!', '');
+}
