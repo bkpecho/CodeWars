d905845f87b8d582b8b5e809805efba4cfb574a5
BK Pecho <bkpecho05@gmail.com>
2023-03-04 10:23:07 +0800

    addItemToAnArray

---
+++ addItemToAnArray.js
@@ -0,0 +1,13 @@
+/* 
+
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+var websites = [];
+
+*/
+
+// add the value "codewars" to the already defined websites array
+websites.push('codewars');
