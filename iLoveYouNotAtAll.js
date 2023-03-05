04989a1bfe3efa4e6d2f929103610553d10cd792
BK Pecho <bkpecho05@gmail.com>
2023-03-05 13:17:08 +0800

    iLoveYouNotAtAll

---
+++ iLoveYouNotAtAll.js
@@ -0,0 +1,35 @@
+/* 
+
+Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+"I love you"
+"a little"
+"a lot"
+"passionately"
+"madly"
+"not at all"
+If there are more than 6 petals, you start over with "I love you" for 7 petals, "a little" for 8 petals and so on.
+
+When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+Your goal in this kata is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. The number of petals is always greater than 0.
+
+*/
+
+function howMuchILoveYou(nbPetals) {
+  let phrases = [
+    'I love you',
+    'a little',
+    'a lot',
+    'passionately',
+    'madly',
+    'not at all'
+  ];
+
+  return phrases[(nbPetals % phrases.length) - 1] === undefined
+    ? phrases[5]
+    : phrases[(nbPetals % phrases.length) - 1];
+}
+
+
+// or you could just do nbpetals -1 % 6
