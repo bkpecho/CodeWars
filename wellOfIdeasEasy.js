a133931eb070a63d8e71d32294c379f746a59132
BK Pecho <bkpecho05@gmail.com>
2023-03-08 18:49:46 +0800

    Update wellOfIdeasEasy

--- wellOfIdeasEasy.js
+++ wellOfIdeasEasy.js
@@ -1,12 +1,8 @@
 /* 
 
-function well(x){
-  
-  let good = 0
-  let bad = 0
-  x.forEach(x => x === 'good' ? good++ : bad++)
-return  good > 2 ? 'I smell a series!' : good >= 1 ? 'Publish!' : 'Fail!'
-}
+For every good kata idea there seem to be quite a few bad ones!
+
+In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
 
 */
 
