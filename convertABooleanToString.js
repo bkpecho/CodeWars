c743c7a77e7461782bc4d208aed2f3a0921140b0
BK Pecho <bkpecho05@gmail.com>
2023-03-11 07:48:52 +0800

    convertABooleanToString

---
+++ convertABooleanToString.js
@@ -0,0 +1,9 @@
+/* 
+
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+*/
+
+const booleanToString = (b) => String(b);
