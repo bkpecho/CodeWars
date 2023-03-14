0df9a2648f5433a20f96769998073f6776d74d73
BK Pecho <bkpecho05@gmail.com>
2023-03-14 22:55:14 +0800

    complementaryDNA

---
+++ 7-kyu/complementaryDNA.js
@@ -0,0 +1,21 @@
+/* 
+
+Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+Example: (input --> output)
+
+"ATTGC" --> "TAACG"
+"GTAT" --> "CATA"
+
+*/
+
+function DNAStrand(dna){
+    return dna.split('').map(x => x === 'A' ? 'T' : x === 'T' ? 'A' : x === 'G' ? 'C' : 'G').join('')
+  }
+  
