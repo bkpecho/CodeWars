097579bb0f55845481ac0c8597754506556c3c11
BK Pecho <bkpecho05@gmail.com>
2023-03-14 21:47:34 +0800

    allStarCodeChallenge18

---
+++ allStarCodeChallenge18.js
@@ -0,0 +1,18 @@
+/* 
+
+Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+If no occurrences can be found, a count of 0 should be returned.
+
+("Hello", "o")  ==>  1
+("Hello", "l")  ==>  2
+("", "z")       ==>  0
+str_count("Hello", 'o'); // returns 1
+str_count("Hello", 'l'); // returns 2
+str_count("", 'z'); // returns 0
+
+*/
+
+function strCount(str, letter){  
+    return str.split('').filter(x => x === letter).length
+  }
