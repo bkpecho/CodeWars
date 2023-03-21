683db26aac346cd9d760c3dcd4138cb26d320581
BK Pecho <bkpecho05@gmail.com>
2023-03-21 17:21:19 +0800

    whoLikesIt

---
+++ 6-kyu/whoLikesIt.js
@@ -0,0 +1,32 @@
+/* 
+
+You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+
+
+*/
+
+function likes(names) {
+  if (names.length === 0) {
+    return 'no one likes this';
+  } else if (names.length === 1) {
+    return `${names.toString()} likes this`;
+  } else if (names.length === 2) {
+    return `${names[0]} and ${names[1]} like this`;
+  } else if (names.length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  } else {
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+}
+
+//my 100th Kata and kata that took me to Kyu 5 Rank :)
