37d28afbe8bf9a1dac8e9aee36e4feeb061aa2df
BK Pecho <bkpecho05@gmail.com>
2023-03-27 05:58:52 +0800

    digitXDigit

---
+++ 7-kyu/digitXDigit.js
@@ -0,0 +1,22 @@
+/* 
+
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+
+Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
+
+Note: The function accepts an integer and returns an integer.
+
+Happy Coding!
+
+*/
+
+function squareDigits(num) {
+  return Number(
+    String(num)
+      .split('')
+      .map((x) => +x * +x)
+      .join('')
+  );
+}
