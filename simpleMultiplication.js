00f0957721d7d5db0a58f7fdfc7d6625b318d9ba
BK Pecho <bkpecho05@gmail.com>
2023-04-18 17:27:41 +0800

    simpleMultiplication

---
+++ simpleMultiplication.js
@@ -0,0 +1,9 @@
+/* 
+
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+*/
+
+function simpleMultiplication(number) {
+  return number % 2 === 0 ? number * 8 : number * 9;
+}
