afbc639ad6f1975acb9d09cc056980a39b57ed7f
woprandis09 <bkpecho05@gmail.com>
2023-04-19 20:53:14 +0800

    itThisTriangle

---
+++ 7-kyu/isThisTriangle.js
@@ -0,0 +1,14 @@
+/* 
+
+Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
+
+*/
+
+function isTriangle(a, b, c) {
+  let caseA = a + b > c;
+  let caseB = b + c > a;
+  let caseC = c + a > b;
+  return caseA && caseB && caseC;
+}
