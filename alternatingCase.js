54b527fda0b4e9c33e63f0ff40e646638dc07f63
woprandis09 <bkpecho05@gmail.com>
2023-04-20 15:22:49 +0800

    alternatingCase

---
+++ alternatingCase.js
@@ -0,0 +1,28 @@
+/* 
+
+Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+
+"hello world".toAlternatingCase() === "HELLO WORLD"
+"HELLO WORLD".toAlternatingCase() === "hello world"
+"hello WORLD".toAlternatingCase() === "HELLO world"
+"HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
+"12345".toAlternatingCase()       === "12345"                   // Non-alphabetical characters are unaffected
+"1a2b3c4d5e".toAlternatingCase()  === "1A2B3C4D5E"
+"String.prototype.toAlternatingCase".toAlternatingCase() === "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+As usual, your function/method should be pure, i.e. it should not mutate the original string.
+
+
+
+*/
+
+String.prototype.toAlternatingCase = function () {
+  let result = '';
+  for (let i = 0; i < this.length; i++) {
+    if (this[i].charCodeAt() >= 65 && this[i].charCodeAt() <= 90) {
+      result += this[i].toLowerCase();
+    } else {
+      result += this[i].toUpperCase();
+    }
+  }
+  return result;
+};
