0dc4a99c92672262387d4a86bcd8e6e8cff07df4
Bryan King Pecho <bkpecho05@gmail.com>
2023-04-28 08:22:26 +0800

    reverseListOrder

---
+++ reverseListOrder.js
@@ -0,0 +1,13 @@
+/*
+
+In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+Examples (Input -> Output)
+* [1, 2, 3, 4]  -> [4, 3, 2, 1]
+* [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+*/
+
+function reverseList(list) {
+  return list.reverse()
+}
