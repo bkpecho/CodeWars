81565b1dccf8873d28a20c04c870ec1a838a5904
woprandis09 <bkpecho05@gmail.com>
2023-05-01 11:38:08 +0800

    isANumberPrime

---
+++ 6-kyu/isANumberPrime.js
@@ -0,0 +1,32 @@
+/* 
+
+Define a function that takes an integer argument and returns a logical value true or false depending on if the integer is a prime.
+
+Per Wikipedia, a prime number ( or a prime ) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+Requirements
+You can assume you will be given an integer input.
+You can not assume that the integer will be only positive. You may be given negative numbers as well ( or 0 ).
+NOTE on performance: There are no fancy optimizations required, but still the most trivial solutions might time out. Numbers go up to 2^31 ( or similar, depending on language ). Looping all the way up to n, or n/2, will be too slow.
+Example
+is_prime(1) 
+is_prime(2)  
+is_prime(-1) 
+
+*/
+
+function isPrime(num) {
+  if (num < 2) {
+    return false;
+  }
+
+  if (num === 2) return true;
+
+  let sqrt = Math.ceil(Math.sqrt(num));
+
+  for (let i = 2; i <= sqrt; i++) {
+    if (num % i === 0) return false;
+  }
+
+  return true;
+}
