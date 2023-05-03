4cf1aece8946cd90b9d0f134db5d26d2f4e6ac8b
woprandis09 <bkpecho05@gmail.com>
2023-05-03 11:15:13 +0800

    sumOfTheFirstNthTermOfSeries

---
+++ 7-kyu/sumOfTheFirstNthTermOfSeries.js
@@ -0,0 +1,26 @@
+/* 
+
+Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+Rules:
+You need to round the answer to 2 decimal places and return it as String.
+
+If the given value is 0 then it should return 0.00
+
+You will only be given Natural Numbers as arguments.
+
+Examples:(Input --> Output)
+1 --> 1 --> "1.00"
+2 --> 1 + 1/4 --> "1.25"
+5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+*/
+
+function SeriesSum(n) {
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    sum += 1 / (1 + i * 3);
+  }
+  return sum.toFixed(2);
+}
