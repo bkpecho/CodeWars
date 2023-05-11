6a672f993cff5c2b4965c2c871172e47be3d436f
woprandis09 <bkpecho05@gmail.com>
2023-05-11 10:35:15 +0800

    pickASetOfFirstElements

---
+++ pickASetOfFirstElements.js
@@ -0,0 +1,21 @@
+/* 
+
+Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence.
+
+If n == 0 return an empty sequence []
+
+Examples
+var arr = ['a', 'b', 'c', 'd', 'e'];
+first(arr) //=> ['a'];
+first(arr, 2) //=> ['a', 'b']
+first(arr, 3) //=> ['a', 'b', 'c'];
+first(arr, 0) //=> [];
+
+*/
+
+function first(arr, n) {
+  if (n === 0) return [];
+  if (!n) return [arr[0]];
+
+  return arr.slice(0, n);
+}
