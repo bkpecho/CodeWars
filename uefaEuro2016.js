e2447322dead721a397d4e95a9c57fbac7072fdd
woprandis09 <bkpecho05@gmail.com>
2023-05-11 10:31:39 +0800

    uefaEuro2016

---
+++ uefaEuro2016.js
@@ -0,0 +1,19 @@
+/* 
+
+Finish the uefaEuro2016() function so it return string just like in the examples below:
+
+uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
+uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy won!"
+uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw."
+
+*/
+
+function uefaEuro2016(teams, scores) {
+  if (scores[0] > scores[1]) {
+    return `At match ${teams[0]} - ${teams[1]}, ${teams[0]} won!`;
+  } else if (scores[0] < scores[1]) {
+    return `At match ${teams[0]} - ${teams[1]}, ${teams[1]} won!`;
+  } else {
+    return `At match ${teams[0]} - ${teams[1]}, teams played draw.`;
+  }
+}
