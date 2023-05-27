e2123d40ac25b0f4ab1bc6c727b05e07a307dde7
Bryan King Pecho <bkpecho05@gmail.com>
2023-05-27 20:21:40 +0800

    countOddNumbersBelowN

---
+++ countOddNumbersBelowN.js
@@ -0,0 +1,14 @@
+/* 
+
+Given a number n, return the number of positive odd numbers below n, EASY!
+
+Examples (Input -> Output)
+7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+Expect large Inputs!
+
+*/
+
+function oddCount(n) {
+  return Math.floor(n / 2);
+}
