a2ad7bcd3c0c7c295b7270521612913ad0cc6675
Bryan King Pecho <bkpecho05@gmail.com>
2023-05-29 22:13:48 +0800

    getTheNthEvenNumber

---
+++ getTheNthEvenNumber.js
@@ -0,0 +1,17 @@
+/* 
+
+Return the Nth Even Number
+
+Example(Input --> Output)
+
+1 --> 0 (the first even number is 0)
+3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+100 --> 198
+1298734 --> 2597466
+The input will not be 0.
+
+*/
+
+function nthEven(n) {
+  return n * 2 - 2;
+}
