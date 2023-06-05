221c9f2fa798baa66764aa110d5d84f3f43140db
Bryan King Pecho <bkpecho05@gmail.com>
2023-06-05 23:35:08 +0800

    contaminationString

---
+++ contaminationString.js
@@ -0,0 +1,26 @@
+/* 
+
+An AI has infected a text with a character!!
+
+This text is now fully mutated to this character.
+
+If the text or the character are empty, return an empty string.
+There will never be a case when both are empty as nothing is going on!!
+
+Note: The character is a string of length 1 or an empty string.
+
+Example
+text before = "abc"
+character   = "z"
+text after  = "zzz"
+
+*/
+
+function contamination(text, char) {
+  if (text.length === '' || char.length === '') return '';
+
+  return text
+    .split('')
+    .map((x) => char)
+    .join('');
+}
