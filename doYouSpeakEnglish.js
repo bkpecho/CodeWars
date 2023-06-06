f66f791bb78250e334e5171f2956560c1c8d75a3
Bryan King Pecho <bkpecho05@gmail.com>
2023-06-06 20:08:18 +0800

    doYouSpeakEnglish

---
+++ doYouSpeakEnglish.js
@@ -0,0 +1,21 @@
+/* 
+
+Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+
+The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+
+Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+Return value as boolean values, true for the string to contains "English", false for it does not.
+
+*/
+
+function spEng(sentence) {
+  let str = sentence.toLowerCase();
+  for (let i = 0; i <= sentence.length - 7; i++) {
+    if (str.slice(i, i + 7) === 'english') {
+      return true;
+    }
+  }
+  return false;
+}
