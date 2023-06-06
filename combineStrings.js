dc9beeeaa9498690ab3816e3804a31fdb1fec345
Bryan King Pecho <bkpecho05@gmail.com>
2023-06-06 20:17:28 +0800

    combineStrings

---
+++ combineStrings.js
@@ -0,0 +1,14 @@
+/* 
+
+Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+
+combineNames('James', 'Stevens')
+returns:
+
+'James Stevens'
+
+*/
+
+const combineNames = (s1, s2) => `${s1} ${s2}`;
