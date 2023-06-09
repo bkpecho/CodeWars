18dab0f0c9b9d9eb18e6ee5e266d7081291d1ce0
Bryan King Pecho <bkpecho05@gmail.com>
2023-06-09 23:22:22 +0800

    takeTheFirstNElements

---
+++ takeTheFirstNElements.js
@@ -0,0 +1,9 @@
+/* 
+
+Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+*/
+
+function take(arr, n) {
+  return arr.slice(0, n);
+}
