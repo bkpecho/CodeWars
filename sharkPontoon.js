2c8a97365a9b37d084acd1597c570d346d054585
Bryan King Pecho <bkpecho05@gmail.com>
2023-06-10 22:04:52 +0800

    sharkPontoon

---
+++ sharkPontoon.js
@@ -0,0 +1,32 @@
+/* 
+
+As you plunge out of the slide into the water, you see a shark hovering in the darkness under the pontoon... Crap!
+
+You need to work out if the shark will get to you before you can get to the pontoon. To make it easier... as you do the mental calculations in the water you either freeze when you realise you are dead, or swim when you realise you can make it!
+
+You are given 5 variables;
+
+sharkDistance = distance from the shark to the pontoon. The shark will eat you if it reaches you before you escape to the pontoon.
+
+sharkSpeed = how fast it can move in metres/second.
+
+pontoonDistance = how far you need to swim to safety in metres.
+
+youSpeed = how fast you can swim in metres/second.
+
+dolphin = a boolean, if true, you can half the swimming speed of the shark as the dolphin will attack it.
+
+The pontoon, you, and the shark are all aligned in one dimension.
+
+If you make it, return "Alive!", if not, return "Shark Bait!".
+
+*/
+
+function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin) {
+  sharkSpeed = dolphin ? sharkSpeed / 2 : sharkSpeed;
+
+  let shark = sharkDistance / sharkSpeed;
+  let you = pontoonDistance / youSpeed;
+
+  return shark > you ? 'Alive!' : 'Shark Bait!';
+}
