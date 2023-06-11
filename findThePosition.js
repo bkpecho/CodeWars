1c5cc65eeeced00706172d372cad96d0a60b6575
Bryan King Pecho <bkpecho05@gmail.com>
2023-06-11 23:34:12 +0800

    findThePosition

---
+++ findThePosition.js
@@ -0,0 +1,11 @@
+/* 
+
+When provided with a letter, return its position in the alphabet.
+
+Input :: "a"
+
+Ouput :: "Position of alphabet: 1"
+
+*/
+
+position = (l) => `Position of alphabet: ${l.charCodeAt() - 96}`;
