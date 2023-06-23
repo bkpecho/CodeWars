3af41e847dec5f28efe7f8a360a75cbfc8bb22ac
Bryan King Pecho <bkpecho05@gmail.com>
2023-06-23 08:24:57 +0800

    findTheRemainder

---
+++ findTheRemainder.js
@@ -0,0 +1,28 @@
+/* 
+
+Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+Division by zero should return NaN.
+
+Examples:
+n = 17
+m = 5
+result = 2 (remainder of `17 / 5`)
+
+n = 13
+m = 72
+result = 7 (remainder of `72 / 13`)
+
+n = 0
+m = -1
+result = 0 (remainder of `0 / -1`)
+
+n = 0
+m = 1
+result - division by zero (refer to the specifications on how to handle this in your language)
+
+*/
+
+function remainder(n, m) {
+  return n > m ? n % m : m % n;
+}
