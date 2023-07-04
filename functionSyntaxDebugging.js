2c62bf96738f56f4bb920346ff81beff2356a1ac
Bryan King Pecho <bkpecho05@gmail.com>
2023-07-04 23:49:03 +0800

    functionSyntaxDebugging

---
+++ functionSyntaxDebugging.js
@@ -0,0 +1,11 @@
+/* 
+
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+*/
+
+function main(verb, noun) {
+  verb = verb ? verb.trim() : verb;
+  noun = noun ? noun.trim() : noun;
+  return verb + ' ' + noun;
+}
