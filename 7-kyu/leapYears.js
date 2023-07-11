004b1b308bc3dc5d0a1acbf9d52284615afb3292
Bryan King Pecho <bkpecho05@gmail.com>
2023-07-11 23:05:43 +0800

    leapYears

---
+++ 7-kyu/leapYears.js
@@ -0,0 +1,25 @@
+/* 
+
+In this kata you should simply determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
+
+years divisible by 4 are leap years
+but years divisible by 100 are not leap years
+but years divisible by 400 are leap years
+Additional Notes:
+
+Only valid years (positive integers) will be tested, so you don't have to validate them
+Examples can be found in the test fixture.
+
+*/
+
+function isLeapYear(year) {
+  if (year % 400 === 0) {
+    return true;
+  } else if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  }
+
+  return false;
+}
